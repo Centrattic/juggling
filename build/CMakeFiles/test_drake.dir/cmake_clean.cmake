eae1ce69a5ca6cfc1a15ae28f0d6dcc4f490fbee
root <root@DESKTOP-FMR7PPV.>
2025-11-22 12:51:25 -0500

    initial test

---
+++ build/CMakeFiles/test_drake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_drake.dir/test_drake.cc.o"
+  "CMakeFiles/test_drake.dir/test_drake.cc.o.d"
+  "test_drake"
+  "test_drake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_drake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
